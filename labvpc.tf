eead7871b37a4ea1824f6d49116b00f300e1ae45
sambyers <samwbyers@gmail.com>
2020-04-04 13:36:51 -0400

    added sg creation to tf config

--- labvpc.tf
+++ labvpc.tf
@@ -1,23 +1,24 @@
+# Var of External IP of FW
 variable "address" {
   type = string
 }
-
+# Vars for Ansible to use when configuring our FW
 data "template_file" "ansible_vars" {
   template = "${file("templates/ansible_vars.yaml.tpl")}"
   vars = {
-    vpc_id = aws_vpc.homelab.id
-    vpn_id = aws_vpn_connection.homelab.id
-    tunnel1_address = aws_vpn_connection.homelab.tunnel1_address
-    tunnel1_preshared_key = aws_vpn_connection.homelab.tunnel1_preshared_key
-    tunnel1_vgw_inside_address = aws_vpn_connection.homelab.tunnel1_vgw_inside_address
-    tunnel1_cgw_inside_address = aws_vpn_connection.homelab.tunnel1_cgw_inside_address
-    tunnel1_bgp_asn = aws_vpn_connection.homelab.tunnel1_bgp_asn
-    bgp_asn = aws_customer_gateway.homelab.bgp_asn
-    tunnel2_address = aws_vpn_connection.homelab.tunnel2_address
-    tunnel2_preshared_key = aws_vpn_connection.homelab.tunnel2_preshared_key
-    tunnel2_vgw_inside_address = aws_vpn_connection.homelab.tunnel2_vgw_inside_address
-    tunnel2_cgw_inside_address = aws_vpn_connection.homelab.tunnel2_cgw_inside_address
-    tunnel2_bgp_asn = aws_vpn_connection.homelab.tunnel2_bgp_asn
+    vpc_id = aws_vpc.lab.id
+    vpn_id = aws_vpn_connection.lab.id
+    tunnel1_address = aws_vpn_connection.lab.tunnel1_address
+    tunnel1_preshared_key = aws_vpn_connection.lab.tunnel1_preshared_key
+    tunnel1_vgw_inside_address = aws_vpn_connection.lab.tunnel1_vgw_inside_address
+    tunnel1_cgw_inside_address = aws_vpn_connection.lab.tunnel1_cgw_inside_address
+    tunnel1_bgp_asn = aws_vpn_connection.lab.tunnel1_bgp_asn
+    bgp_asn = aws_customer_gateway.lab.bgp_asn
+    tunnel2_address = aws_vpn_connection.lab.tunnel2_address
+    tunnel2_preshared_key = aws_vpn_connection.lab.tunnel2_preshared_key
+    tunnel2_vgw_inside_address = aws_vpn_connection.lab.tunnel2_vgw_inside_address
+    tunnel2_cgw_inside_address = aws_vpn_connection.lab.tunnel2_cgw_inside_address
+    tunnel2_bgp_asn = aws_vpn_connection.lab.tunnel2_bgp_asn
   }
 }
 
@@ -31,48 +32,82 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-resource "aws_vpc" "homelab" {
+resource "aws_vpc" "lab" {
   cidr_block = "172.16.0.0/16"
 
   tags = {
-    Name        = "Home Lab VPC"
+    Name        = "Lab_VPC"
     Environment = "Lab"
   }
 }
 
-resource "aws_customer_gateway" "homelab" {
+resource "aws_security_group" "lab" {
+  name        = "lab default sg"
+  description = "Allow SSH and ICMP"
+  vpc_id      = aws_vpc.lab.id
+
+  ingress {
+    description = "SSH from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.lab.cidr_block]
+  }
+
+  ingress {
+    description = "SSH from on premise network"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["192.168.0.0/16"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "Lab_SG"
+    Environment = "Lab"
+  }
+}
+
+resource "aws_customer_gateway" "lab" {
   bgp_asn    = 65000
   ip_address = var.address
   type       = "ipsec.1"
 
   tags = {
-    Name        = "Home Lab Cu GW"
+    Name        = "Lab_CU_GW"
     Environment = "Lab"
   }
 }
 
-resource "aws_vpn_gateway" "homelab" {
-  vpc_id          = aws_vpc.homelab.id
+resource "aws_vpn_gateway" "lab" {
+  vpc_id          = aws_vpc.lab.id
   amazon_side_asn = 64512
 
   tags = {
-    Name        = "Home Lab VPN GW"
+    Name        = "Lab_VPN_GW"
     Environment = "Lab"
   }
 }
 
-resource "aws_vpn_connection" "homelab" {
-  vpn_gateway_id      = aws_vpn_gateway.homelab.id
-  customer_gateway_id = aws_customer_gateway.homelab.id
+resource "aws_vpn_connection" "lab" {
+  vpn_gateway_id      = aws_vpn_gateway.lab.id
+  customer_gateway_id = aws_customer_gateway.lab.id
   type                = "ipsec.1"
 
   tags = {
-    Name        = "Home Lab VPN"
+    Name        = "Lab_VPN"
     Environment = "Lab"
   }
 }
 
-resource "aws_vpn_gateway_route_propagation" "homelab" {
-  vpn_gateway_id = aws_vpn_gateway.homelab.id
-  route_table_id = aws_vpc.homelab.default_route_table_id
+resource "aws_vpn_gateway_route_propagation" "lab" {
+  vpn_gateway_id = aws_vpn_gateway.lab.id
+  route_table_id = aws_vpc.lab.default_route_table_id
 }
